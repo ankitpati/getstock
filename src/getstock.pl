ec8f20b043e43b32ea6ae67febf4b45d7f52a300
Ankit Pati <ankitpati@gmail.com>
2017-01-25 12:07:25 +0530

    subroutine get_stock_prices for modularity and testability

--- src/getstock.pl
+++ src/getstock.pl
@@ -8,9 +8,13 @@ use LWP::Simple;
 die "Usage:\n\tgetstock.pl -s <ticker>...\n"
                                         if (@ARGV <= 1 || shift @ARGV ne '-s');
 
-my $request_url = 'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=';
-$request_url .= uc "$_," foreach (@ARGV);
+sub get_stock_prices {
+    my $request_url = 'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=';
+    $request_url .= uc "$_," foreach (@_);
 
-my $stock_prices = get($request_url) or die "Could not contact servers.\n";
+    my $stock_prices = get($request_url) or die "Could not contact servers.\n";
 
-print $stock_prices;
+    return $stock_prices;
+}
+
+print get_stock_prices @ARGV;
