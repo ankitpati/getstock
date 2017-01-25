ce877fa0fe879861406097e023ddc7f6c5e51926
Ankit Pati <ankitpati@gmail.com>
2017-01-25 12:10:34 +0530

    separated core function into module for testability

---
+++ src/Getstock.pm
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use LWP::Simple;
+
+sub get_stock_prices {
+    my $request_url = 'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=';
+    $request_url .= uc "$_," foreach (@_);
+
+    my $stock_prices = get($request_url) or die "Could not contact servers.\n";
+
+    return $stock_prices;
+}
+
+1;
--- src/getstock.pl
+++ src/getstock.pl
@@ -4,17 +4,9 @@ use strict;
 use warnings;
 
 use LWP::Simple;
+use Getstock;
 
 die "Usage:\n\tgetstock.pl -s <ticker>...\n"
                                         if (@ARGV <= 1 || shift @ARGV ne '-s');
-
-sub get_stock_prices {
-    my $request_url = 'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=';
-    $request_url .= uc "$_," foreach (@_);
-
-    my $stock_prices = get($request_url) or die "Could not contact servers.\n";
-
-    return $stock_prices;
-}
 
 print get_stock_prices @ARGV;
