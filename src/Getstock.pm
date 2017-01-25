daa6c730398888efc246982a9d0b3de07833e8fa
Ankit Pati <ankitpati@gmail.com>
2017-01-25 12:24:19 +0530

    simplified get calling syntax

--- src/Getstock.pm
+++ src/Getstock.pm
@@ -9,7 +9,7 @@ sub get_stock_prices {
     my $request_url = 'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=';
     $request_url .= uc "$_," foreach (@_);
 
-    my $stock_prices = get($request_url) or die "Could not contact servers.\n";
+    my $stock_prices = get $request_url or die "Could not contact servers.\n";
 
     return $stock_prices;
 }
