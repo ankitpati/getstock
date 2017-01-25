2f0e450bde3af58ec7ad1b11a15d61eb0c577331
Ankit Pati <ankitpati@gmail.com>
2017-01-25 18:21:02 +0530

    simplified method calling syntax

--- src/main.pl
+++ src/main.pl
@@ -20,13 +20,13 @@ eval {
     if ($method eq '-c') {
         my $stock_prices = new Csvstock(
             'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=',
-            @ARGV)->csv_stock_prices();
+            @ARGV)->csv_stock_prices;
         print $stock_prices;
     }
     elsif ($method eq '-s') {
         my $stock_prices = new Scrapestock('http://finance.yahoo.com/quote/',
             '<span class="Fw\(b\) Fz\(36px\) Mb\(-4px\)" data-reactid="(?:\d+)">(.*?)<\/span>',
-            @ARGV)->scrape_stock_prices();
+            @ARGV)->scrape_stock_prices;
         print $stock_prices;
     }
     else {
