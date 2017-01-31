7d2c38ad8024c3a54375a5840c158b80e35ee5ca
Ankit Pati <ankitpati@gmail.com>
2017-01-31 14:22:34 +0530

    consolidated redundant code

--- src/main.pl
+++ src/main.pl
@@ -17,19 +17,21 @@ die "$usage\n" if (@ARGV <= 1);
 my $method = shift @ARGV;
 
 eval {
+    my $stock_prices;
+
     if ($method eq '-c') {
-        my $stock_prices = new Csvstock(
+        $stock_prices = new Csvstock(
             'http://download.finance.yahoo.com/d/quotes.csv?f=l1&s=')
             ->csv_stock_prices (@ARGV);
-        print $stock_prices;
     }
     elsif ($method eq '-s') {
-        my $stock_prices = new Scrapestock('https://finance.yahoo.com/quote/',
+        $stock_prices = new Scrapestock('https://finance.yahoo.com/quote/',
             '<span class="Fw\(b\) Fz\(36px\) Mb\(-4px\)" data-reactid="(?:\d+)">(.*?)<\/span>')
             ->scrape_stock_prices (@ARGV);
-        print $stock_prices;
     }
     else {
         die "$usage\n";
     }
+
+    print $stock_prices;
 } or die "$@\n";
